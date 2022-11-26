52d275407d44ca0dfa32cc694026dde117e880af
Yash-wardhan <56475291+Yash-wardhan@users.noreply.github.com>
2022-11-26 15:53:37 +0530

    First commit

---
+++ listings/choices.py
@@ -0,0 +1,51 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'RS100,000',
+  '200000':'RS200,000',
+  '300000':'RS300,000',
+  '400000':'RS400,000',
+  '500000':'RS500,000',
+  '600000':'RS600,000',
+  '700000':'RS700,000',
+  '800000':'RS800,000',
+  '900000':'RS900,000',
+  '1000000':'RS1cr+',
+}
+
+state_choices = {
+        
+       'AP': 'Andra Pradesh',
+       'HYD' : 'Hyderabad' ,
+       'AP' : 'Arunachal Pradesh',
+       'AS':'Assam',
+       'BH':'Bihar',
+       'P':'Patna',
+       'CHG':'Chhattisgarh',
+       'Rp':'Raipur',
+       'G': 'Goa',
+       'GU':'Gujarat',
+       'H': 'Haryana',
+       'HP': 'Himachal Pradesh',
+       'JAK'  : 'Jammu and Kashmir',
+       'JK': 'Jharkhand',
+       'RCH': 'Ranchi',
+       'KR': 'Karnataka',
+       'KRL' : 'Kerala',
+       'MP' : 'Madya Pradesh',
+       'BP' :  'Bhopal',
+       'MP':'Maharashtra',
+       'OD' :  'Orissa'
+       
+}
